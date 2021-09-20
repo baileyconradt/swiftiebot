88589cc42188758b5c82751d65d02bfd6a0b05a2
Bailey Conradt <baileyconradt@gmail.com>
2021-09-20 16:36:30 -0500

    happy images

--- bot.js
+++ bot.js
@@ -12,6 +12,36 @@ client.stream('statuses/filter', { track: '@swiftie_bot' }, function (stream) {
 
     console.log("Replying with Taylor pic!");
 
+    var happyTerms = [
+      'love',
+      'like',
+      'excited',
+      'cool',
+      'awesome',
+      'escstatic',
+      'glad',
+      'happy',
+      'sweet',
+      'wholesome',
+      'fantastic',
+      'amazing',
+      'incredible',
+      'queen'
+    ]
+
+    var happyPicArray = [
+      'happyTaylor.jpeg',
+      'heartTaylor.jpeg',
+      'heart2Taylor.jpeg',
+      'heart3Taylor.jpeg',
+      'heart4Taylor.jpeg',
+      'heart5Taylor.jpeg',
+      'heart6Taylor.jpeg',
+      'heart7Taylor.jpeg',
+      'heart8Taylor.jpeg',
+      'loveTaylor.jpeg',
+    ];
+
     var picArray = [
       'annoyedTaylor.jpeg',
       'confusedTaylor.jpeg',
@@ -20,15 +50,22 @@ client.stream('statuses/filter', { track: '@swiftie_bot' }, function (stream) {
       'heartTaylor.jpeg',
       'loveTaylor.jpeg',
       'thinkingTaylor.jpeg'
-  ];
-  var randomNumber = Math.floor(Math.random()*picArray.length);
+    ];
 
-  var picPath = picArray[randomNumber];
+    var randomNumber = Math.floor(Math.random() * picArray.length);
+    var picPath = picArray[randomNumber];
+
+    //code to return positive images for positive text
+    if(happyTerms.some(term => tweet.text.includes(term))){
+      var randomNumber2 = Math.floor(Math.random() * happyPicArray.length);
+
+      picPath = happyPicArray[randomNumber2];
+    }
 
     postReplyWithMedia(client, "./sample-media/" + picPath, tweet);
 
     stream.on('error', function (error) {
-    console.log(error);
+      console.log(error);
+    });
   });
-});
 });
