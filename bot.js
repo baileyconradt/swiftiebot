793395db97df1b09288ffb0d8ccecac78ba633b6
Bailey Conradt <baileyconradt@gmail.com>
2021-09-26 15:24:41 -0500

    fixed file paths

--- bot.js
+++ bot.js
@@ -4,13 +4,19 @@ const client = auth();
 
 const fs = require('fs');
 
+
+//TODO: 
+//code to return album-specific images
+
+//code to return random quote messages based on sentiment
+
+//random lyrics every 22 min
+
 client.stream('statuses/filter', { track: '@swiftie_bot' }, function (stream) {
   console.log("Searching for tweets...");
 
   var Sentiment = require('sentiment');
   var sentiment = new Sentiment();
-  //var result = sentiment.analyze('Cats are stupid.');
-  //console.dir(result.score);    // Score: -2, Comparative: -0.666
 
   // when a tweet is found
   stream.on('data', function (tweet) {
@@ -19,65 +25,44 @@ client.stream('statuses/filter', { track: '@swiftie_bot' }, function (stream) {
 
     console.log("Replying with Taylor pic!");
 
-    const dir = './happyPics';
-    let happyLength = 0;
+    const getRandomIndex = length => Math.floor(Math.random() * length);
 
-    fs.readdir(dir, (err, files) => {
-      happyLength = files.length;
-    });
-
-    const dir2 = './sadPics';
-    let sadLength = 0;
-
-    fs.readdir(dir2, (err, files) => {
-      sadLength = files.length;
-    });
-
-    var happyPicArray = [
-      'happyTaylor.jpeg',
-      'heartTaylor.jpeg',
-      'heart2Taylor.jpeg',
-      'heart3Taylor.jpeg',
-      'heart4Taylor.jpeg',
-      'heart5Taylor.jpeg',
-      'heart6Taylor.jpeg',
-      'heart7Taylor.jpeg',
-      'heart8Taylor.jpeg',
-      'loveTaylor.jpeg',
-    ];
-
-    var picArray = [
-      'annoyedTaylor.jpeg',
-      'confusedTaylor.jpeg',
-      'drunkTaylor.jpeg',
-      'thinkingTaylor.jpeg'
-    ];
-
-    var randomNumber;
-    // = Math.floor(Math.random() * picArray.length);
-    var picPath;
-    // = picArray[randomNumber];
-
+    //get tweet sentiment
     var result = sentiment.analyze(tweet.text);
+    //placeholder for image path
+    var picPath;
 
     //code to return positive images for positive text
-    //if(happyTerms.some(term => tweet.text.includes(term))){
-    if(result.score > 0){
-      var randomNumber2 = Math.floor(Math.random() * happyLength);
-      console.log('Taylor likes this one! <3');
-      picPath = './happyPics/' + happyPicArray[randomNumber2];
+    if (result.score > 0) {
+      const dir = './happyPics';
+      let happyLength = 0;
+      fs.readdir(dir, (err, files) => {
+        happyLength = files.length;
+
+        let index = getRandomIndex(happyLength);
+        console.log('Taylor likes this one! <3');
+        //console.log(files[index]);
+        //console.log(dir + '/' + files[index]);
+        picPath = dir + '/' + files[index]
+
+        postReplyWithMedia(client, picPath, tweet);
+      });
     }
-    else{
-      console.log('Taylor does not like this one! >:(');
-      randomNumber = Math.floor(Math.random() * sadLength);
-      picPath = './sadPics/' + picArray[randomNumber];
+    else {
+      const dir = './sadPics';
+      let sadLength = 0;
+      fs.readdir(dir, (err, files) => {
+        sadLength = files.length;
+
+        let index = getRandomIndex(sadLength);
+        console.log('Taylor doesn\'t like this one :(');
+        //console.log(files[index]);
+        //console.log(dir + '/' + files[index]);
+        picPath = dir + '/' + files[index]
+
+        postReplyWithMedia(client, picPath, tweet);
+      });
     }
-
-    //code to return album-specific images
-
-
-
-    postReplyWithMedia(client, picPath, tweet);
 
     stream.on('error', function (error) {
       console.log(error);
